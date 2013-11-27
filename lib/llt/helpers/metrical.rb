9ef975cc31f83f6b49e9cfa849364fcf1d8efd8b
LFDM <1986gh@gmail.com>
2013-11-27 22:25:43 +0100

    Adds map for quantified chars.

--- lib/llt/helpers/metrical.rb
+++ lib/llt/helpers/metrical.rb
@@ -1,9 +1,18 @@
 module LLT
   module Helpers
     module Metrical
-      QUANTIFIED_CHARS = /[ăāĕēĭīŏōŭūўȳ]/
+      QUANTIFIED_CHARS = {
+        'a' => ['ă', 'ā'],
+        'e' => ['ĕ', 'ē'],
+        'i' => ['ĭ', 'ī'],
+        'o' => ['ŏ', 'ō'],
+        'u' => ['ŭ', 'ū'],
+        'y' => ['ў', 'ȳ'],
+      }
+      QUANTIFIED_CHARS_REGEXP = /[#{QUANTIFIED_CHARS.values.flatten.join}]/
+
       def evaluate_metrical_presence(string)
-        @metrical = string && string.match(QUANTIFIED_CHARS)
+        @metrical = string && string.match(QUANTIFIED_CHARS_REGEXP)
       end
 
       def metrical?
